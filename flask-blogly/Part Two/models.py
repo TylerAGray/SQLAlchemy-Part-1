10177e0a47e678169df5fbb1c9e408186fb67eb5
TylerAGray <163683666+TylerAGray@users.noreply.github.com>
2024-07-24 08:43:29 -0700

    models.py

--- flask-blogly/Part Two/models.py
+++ flask-blogly/Part Two/models.py
@@ -51,8 +51,6 @@ class Post(db.Model):
 
 def connect_db(app):
     """Connect this database to provided Flask app.
-
-    You should call this in your Flask app.
     """
     db.app = app
     db.init_app(app)
