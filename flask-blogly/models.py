211993b72ff096bc409011d6ee3ef55c85e18d84
TylerAGray <163683666+TylerAGray@users.noreply.github.com>
2024-07-24 08:43:54 -0700

    models.py

--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -24,8 +24,6 @@ class User(db.Model):
 
 def connect_db(app):
     """Connect this database to provided Flask app.
-
-    You should call this in your Flask app.
     """
 
     # Bind the Flask app to the SQLAlchemy object
